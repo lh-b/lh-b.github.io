0d8ef8933db7abb47ad89951b865481431c4785b
Huibo Ly <88732489+lh-b@users.noreply.github.com>
2021-08-23 18:09:20 +0900

    Delete _apps directory

--- _apps/macos/static/chunks/pages/_components/Dock-80a38357ee23d6972cf3.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4],{7411:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_components/Dock",function(){return u(9492)}])}},function(n){n.O(0,[885,166,774,888,179],(function(){return _=7411,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
--- _apps/macos/static/chunks/pages/_components/MenuBar-ccc74781a9eb76eb7935.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[213],{5298:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_components/MenuBar",function(){return u(1453)}])}},function(n){n.O(0,[885,166,774,888,179],(function(){return _=5298,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
