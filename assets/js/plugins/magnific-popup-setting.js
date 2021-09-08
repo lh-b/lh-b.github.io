6beed34183ef2bc4f94e20a73a47875fb3742f47
Huibo Ly <88732489+lh-b@users.noreply.github.com>
2021-09-08 12:26:53 +0900

    이미지 클릭시 팝업 활성화

---
+++ assets/js/plugins/magnific-popup-setting.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.page__content img').wrap( function(){
+        $(this).magnificPopup({
+            type: 'image',
+            closeOnContentClick: true,
+            showCloseBtn: false,
+            items: {
+              src: $(this).attr('src')
+            },
+        });
+        $(this).parent('p').css('overflow', 'auto');
+        return '<a href="' + $(this).attr('src') + '" style="width:' + $(this).attr('width') + 'px;"><figure> </figure>' + '<figcaption style="text-align: center;" class="caption">' + $(this).attr('alt') + '</figcaption>' + '</a>';
+    });
+});
