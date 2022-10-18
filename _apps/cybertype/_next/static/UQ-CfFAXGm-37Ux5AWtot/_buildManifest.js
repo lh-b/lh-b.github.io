e4280aab70851ebe2d887ed14e70a590872e7c68
Huibo Ly <88732489+lh-b@users.noreply.github.com>
2022-10-18 21:12:10 +0900

    Delete _apps/cybertype/_next/static directory

--- _apps/cybertype/_next/static/UQ-CfFAXGm-37Ux5AWtot/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/692-b5bc129a2991b82b.js","static/css/43b9a685781f8208.css","static/chunks/pages/index-166fdcaded8ef250.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
